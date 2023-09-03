82136d3146a5c1e530bbccd24fd069ce36a3eaa5
NastassiaRamanovich <nastassia_ramanovich@mail.ru>
2023-09-03 22:08:26 +0300

    Количество четных чисел в массиве

---
+++ Sem5_Task34/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 34: Задайте массив заполненный случайными 
+// положительными трёхзначными числами. Напишите программу, 
+// которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+void fillArray(int[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+        array[i] = new Random().Next(100, 1000);       
+  }
+  Console.WriteLine("[{0}]", string.Join(", ", array));
+}
+int[]array = new int[8];
+fillArray(array);
+
+void CountNumbers(int [] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        count = count + 1;
+    }
+    Console.WriteLine("Количество четных чисел в массиве: " + count);
+} 
+
+CountNumbers(array);
