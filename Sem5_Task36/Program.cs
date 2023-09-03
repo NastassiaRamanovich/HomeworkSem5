33040cd7c6ff8f4ca1514c5d041c46156fb5806e
NastassiaRamanovich <nastassia_ramanovich@mail.ru>
2023-09-03 22:18:53 +0300

    Сумма чисел на четных позициях в массиве

---
+++ Sem5_Task36/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный 
+// случайными числами. Найдите сумму элементов, стоящих 
+// на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+void fillArray(int[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+        array[i] = new Random().Next(1, 100);       
+  }
+  Console.WriteLine("[{0}]", string.Join(", ", array));
+}
+int[]array = new int[8];
+fillArray(array);
+
+void SumNumbers(int [] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 0)
+        sum = sum + array[i];
+    }
+    Console.WriteLine("Сумма чисел на четных позициях в массиве равна " + sum);
+} 
+
+SumNumbers(array);
