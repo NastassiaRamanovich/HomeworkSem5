19eafc5eacf407d11c4cd5396fcfe53488953ae9
NastassiaRamanovich <nastassia_ramanovich@mail.ru>
2023-09-05 20:32:32 +0300

    Вывод двухмерного массива Аmn = m+n

---
+++ Sem7_Task48/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, 
+// каждый элемент в массиве находится по формуле: Aₘₙ = m+n. 
+// Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int m = 3;
+        int n = 4;
+
+        int[,] array = new int[m, n];
+
+        // Заполнение массива по формуле Aₘₙ = m+n
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                array[i, j] = i + j;
+            }
+        }
+
+        // Вывод массива на экран
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
