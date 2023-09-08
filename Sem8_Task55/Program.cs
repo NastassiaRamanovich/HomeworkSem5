5a0062855d4ab7cb3ee3b5d065881b26d393f117
NastassiaRamanovich <nastassia_ramanovich@mail.ru>
2023-09-08 21:11:37 +0300

    Перевернуть массив

---
+++ Sem8_Task55/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу, 
+// которая заменяет строки на столбцы. В случае, если это невозможно, 
+// программа должна вывести сообщение для пользователя.
+
+int[,] array = new int[4, 5];
+
+int[,] FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-10, 10);
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    return array;
+}
+
+int[,] array2 = FillArray(array);
+
+Console.WriteLine("---------------");
+
+
+void PrintArray(int[,] array){
+    if (array.GetLength(0) != array.GetLength(1))
+    {
+        Console.WriteLine("Невозможно перевернуть массив");
+    }
+    else
+    for (int j = 0; j < array.GetLength(0); j++)
+    {
+        for (int i = 0; i < array.GetLength(1); i++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintArray(array2);
+
